8c89850e72974817630c0fefa45033e24945d3c6
venkat <devopsaws84s@gmail.com>
2025-12-27 19:45:14 +0530

    Terraform

--- Variables.tf
+++ Variables.tf
@@ -8,6 +8,14 @@ variable "instance_type" {
     default = "t3.micro"
     type = string
     description = "INSTANCE SIZE"
+
+    #We can write validation here to use only these 3 type of instances sizes, Other than taht we cannot create
+    
+    validation {
+       condition = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
+       error_message = "Valid Values for instance size are : t3.micro, t3.small, t3.medium"
+    }
+
 }
 
 #Mandatory to provide

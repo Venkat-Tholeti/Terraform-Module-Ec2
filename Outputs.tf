dcb94aceb1a06d79829aa94d554113e1244203d4
venkat <devopsaws84s@gmail.com>
2025-12-27 20:22:24 +0530

    Terraform

--- Outputs.tf
+++ Outputs.tf
@@ -1,16 +1,11 @@
 output "public_ip" {
-  value       = "aws_instance.main.public_ip"
-  description = "The Public IP address of the server."
+  value       = aws_instance.main.public_ip
 }
-
 
 output "private_ip" {
-  value       = "aws_instance.main.private_ip"
-  description = "The Private IP address of the server."
+  value       = aws_instance.main.private_ip
 }
 
-
 output "instance_id" {
-  value       = "aws_instance.main.instance_id"
-  description = "The Instance id  of the server."
+  value       = aws_instance.main.id
 }
